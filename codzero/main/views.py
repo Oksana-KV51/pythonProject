b9484ce36c9e305ecd5d347ac4af094124280b63
Oksana-KV51 <12345-70@mail.ru>
2024-06-19 20:36:32 +0400

    add new

--- codzero/main/views.py
+++ codzero/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1> ура моему первому проекту на DJANGO!<h1>")
+
+def data(request):
+    return HttpResponse("<h1> Добро пожаловать на страницу DATA!<h1>")
+
+def test(request):
+    return HttpResponse("<h1> Добро пожаловать на страницу TEST!<h1>")
+
