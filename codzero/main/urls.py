f1e5c3abedd8179747029c59b1908b3ed0be0274
Oksana-KV51 <12345-70@mail.ru>
2024-06-20 20:42:19 +0400

    add code lesson2

--- codzero/main/urls.py
+++ codzero/main/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('data/', views.data),
-    path('test/', views.test)
-]
+    path('', views.index, name='home'),
+    path('new', views.new, name='page2')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- codzero/main/views.py
+++ codzero/main/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> ура моему первому проекту на DJANGO!<h1>")
+    return render (request, 'main/index.html', {'caption':"CatDjango"})
 
-def data(request):
-    return HttpResponse("<h1> Добро пожаловать на страницу DATA!<h1>")
+#def data(request):
+    #return HttpResponse("<h1> Добро пожаловать на страницу DATA!<h1>")
 
-def test(request):
-    return HttpResponse("<h1> Добро пожаловать на страницу TEST!<h1>")
+def new(request):
+    return render(request, 'main/new.html')
 
