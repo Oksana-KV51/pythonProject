464bc613c880b32fe86441428bab7c2f559d07db
Oksana-KV51 <12345-70@mail.ru>
2024-06-21 20:24:36 +0400

    add DZ

--- codzero/main/urls.py
+++ codzero/main/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('new', views.new, name='page2')
+    path('new', views.new, name='page2'),
+    path('page3', views.new, name='page3'),
+    path('page4', views.new, name='page4')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- codzero/main/views.py
+++ codzero/main/views.py
@@ -3,11 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render (request, 'main/index.html', {'caption':"CatDjango"})
+    return render(request, 'main/index.html')
 
 #def data(request):
     #return HttpResponse("<h1> Добро пожаловать на страницу DATA!<h1>")
 
 def new(request):
     return render(request, 'main/new.html')
+
+def page3(request):
+    return render(request, 'main/page3.html')
+def page4(request):
+    return render(request, 'main/page4.html')
 
